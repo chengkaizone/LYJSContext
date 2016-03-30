d56142260d1f71250fbfef61cc02177112d2a80b
记忆有你 <1094226429@qq.com>
2016-03-30 13:14:47 +0800

    Initial Commit

---
+++ LYJSContext/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYJSContext
+//
+//  Created by joinhov on 16/3/30.
+//  Copyright © 2016年 lance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
